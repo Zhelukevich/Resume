bceff6befb1f5bf3bca5e630d059b0a6fad2f028
Pavel Zhelukevich <pashazhelukevich1@gmail.com>
2023-01-10 18:32:58 +0300

    section

--- js/main.js
+++ js/main.js
@@ -18,20 +18,14 @@ const btnUp = {
     this.el.classList.remove('btn-up_hide');
   },
   hide() {
-    // добавим к кнопке класс btn-up_hide
     this.el.classList.add('btn-up_hide');
   },
   addEventListener() {
-    // при прокрутке содержимого страницы
     window.addEventListener('scroll', () => {
-      // определяем величину прокрутки
       const scrollY = window.scrollY || document.documentElement.scrollTop;
-      // если страница прокручена больше чем на 400px, то делаем кнопку видимой, иначе скрываем
       scrollY > 400 ? this.show() : this.hide();
     });
-    // при нажатии на кнопку .btn-up
     document.querySelector('.btn-up').onclick = () => {
-      // переместим в начало страницы
       window.scrollTo({
         top: 0,
         left: 0,
@@ -41,3 +35,13 @@ const btnUp = {
   }
 }
 btnUp.addEventListener();
+
+// SWIPER
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
