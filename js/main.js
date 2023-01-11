914bfb7c5294609f398454a211686741ec69776b
Pavel Zhelukevich <pashazhelukevich1@gmail.com>
2023-01-11 19:07:21 +0300

    burger

--- js/main.js
+++ js/main.js
@@ -1,47 +1,70 @@
-const body = document.getElementById('body');
-const header = document.getElementById('header');
+document.addEventListener('DOMContentLoaded', function () {
 
-window.addEventListener('scroll', () => {
-  const scrollY = window.scrollY || document.documentElement.scrollTop;
-  if (scrollY > 50) {
-    header.classList.add('black')
-  } else {
-    header.classList.remove('black')
-  }
-});
+  const body = document.getElementById('body');
+  const header = document.getElementById('header');
+
+  window.addEventListener('scroll', () => {
+    const scrollY = window.scrollY || document.documentElement.scrollTop;
+    if (scrollY > 50) {
+      header.classList.add('black')
+    } else {
+      header.classList.remove('black')
+    }
+  });
 
 
-const btnUp = {
-  el: document.querySelector('.btn-up'),
-  show() {
-    // удалим у кнопки класс btn-up_hide
-    this.el.classList.remove('btn-up_hide');
-  },
-  hide() {
-    this.el.classList.add('btn-up_hide');
-  },
-  addEventListener() {
-    window.addEventListener('scroll', () => {
-      const scrollY = window.scrollY || document.documentElement.scrollTop;
-      scrollY > 400 ? this.show() : this.hide();
-    });
-    document.querySelector('.btn-up').onclick = () => {
-      window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: 'smooth'
+  const btnUp = {
+    el: document.querySelector('.btn-up'),
+    show() {
+      // удалим у кнопки класс btn-up_hide
+      this.el.classList.remove('btn-up_hide');
+    },
+    hide() {
+      this.el.classList.add('btn-up_hide');
+    },
+    addEventListener() {
+      window.addEventListener('scroll', () => {
+        const scrollY = window.scrollY || document.documentElement.scrollTop;
+        scrollY > 400 ? this.show() : this.hide();
       });
+      document.querySelector('.btn-up').onclick = () => {
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: 'smooth'
+        });
+      }
     }
   }
-}
-btnUp.addEventListener();
+  btnUp.addEventListener();
 
-// SWIPER
-const swiper = new Swiper(".swiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+  // SWIPER
+  const swiper = new Swiper(".swiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+
+
+  // BURGER
+
+  const burger = document.getElementById('burger');
+  const headerID = document.getElementById('header');
+  const navLink = document.querySelectorAll('.nav__link');
+
+  burger.addEventListener('click', function () {
+    headerID.classList.toggle('open');
+  })
+
+  navLink.forEach(function (el) {
+    el.addEventListener('click', function () {
+      headerID.classList.toggle('open');
+    })
+
+  })
+
+})
